ba858df2802fbd41e45beb46d134364ace3ebe3d
Yogesh Dhangar <vijay45155@gmail.com>
2022-06-22 23:16:44 -0700

    filter bar removed

--- frontend/src/Components/Home/Home.js
+++ frontend/src/Components/Home/Home.js
@@ -1,11 +1,9 @@
 import React from "react";
-import FilterBar from "./FilterBar";
 import DisplayCard from "./DisplayCard";
 
 const Home = () => {
   return (
     <>
-      <FilterBar />
       <DisplayCard />
     </>
   );
